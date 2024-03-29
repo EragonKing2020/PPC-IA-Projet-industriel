1edcba7837dee3694e69a78dec8ab43d986e73bc
EragonKing2020 <Eragonking2020@gmail.com>
2024-03-29 14:20:39 +0100

    Retour avant essais

--- src/main/java/data/Strategie.java
+++ src/main/java/data/Strategie.java
@@ -1,6 +1,5 @@
 package data;
 
-import java.util.HashMap;
 import java.util.LinkedList;
 
 import org.chocosolver.solver.search.strategy.assignments.DecisionOperatorFactory;
@@ -15,9 +14,7 @@ class Strategie extends AbstractStrategy<IntVar>{
 	private int appels = 0;
 	private IntVar prevDecision = null;
 	private Workshop workshop;
-	LinkedList<Node> varsVarsSmallestLB = new LinkedList<Node>();
-	LinkedList<IntVar> ordreInstanciation = new LinkedList<IntVar>();
-	LinkedList<Integer> valInstanciation = new LinkedList<Integer>();
+	
 	
 	public Strategie(IntVar[] decisionVars,Workshop workshop) {
 		super(decisionVars);
@@ -34,41 +31,7 @@ class Strategie extends AbstractStrategy<IntVar>{
 		return tempsApres;
 	}
 	
-	private boolean sameOrderValueUpToL1(LinkedList<IntVar> l1, LinkedList<IntVar> l2) {
-		if (l1.size() > l2.size()) return false;
-		for (int i = 0; i < l1.size(); i ++) {
-			if (l1.get(i) != l2.get(i))
-				return false;
-		}
-		return true;
-	}
-	
-	private boolean sameOrderValue(LinkedList<IntVar> l1, LinkedList<IntVar> l2) {
-		if (l1.size() != l2.size()) return false;
-		for (int i = 0; i < l1.size(); i ++) {
-			if (l1.get(i) != l2.get(i))
-				return false;
-		}
-		return true;
-	}
-	
-	private boolean contains(LinkedList<IntVar> l1, LinkedList<IntVar> l2) {
-		if (l1.size() > l2.size()) return false;
-		for (IntVar var : l1)
-			if (!l2.contains(var))
-				return false;
-		return true;
-	}
-	
-	private LinkedList<Node> getNodesBefore(){
-		LinkedList<Node> nodes = new LinkedList<Node>();
-		for (Node node : varsVarsSmallestLB)
-			if (this.sameOrderValueUpToL1(node.instancies, ordreInstanciation))
-				nodes.add(node);
-		return nodes;
-	}
-	
-	private IntVar[] getUninstanciatedWithSmallestLB() {
+	private IntVar getUninstanciatedWithSmallestLB() {
 		LinkedList<IntVar> varsSmallestLB = new LinkedList<IntVar>();
 		int smallestLB = -1000;
 		for (int i = 0; i < vars.length; i ++) {
@@ -83,20 +46,8 @@ class Strategie extends AbstractStrategy<IntVar>{
 				}
 			}
 		}
-		
 		if (varsSmallestLB.isEmpty())
-			return new IntVar[] {null, null};
-		
-		boolean boolAdd = true;
-		LinkedList<Node> nodesBefore = getNodesBefore();
-		for (int iNode = 0; iNode < nodesBefore.size(); iNode ++) {
-			if (this.contains(varsSmallestLB, nodesBefore.get(iNode).smallestLB)) {
-				if (varsSmallestLB.get(0).getLB() > nodesBefore.get(iNode).valLB) {
-					return new IntVar[] {varsSmallestLB.get(0), null};
-				}
-				boolAdd = false;
-			}
-		}
+			return null;
 		
 		IntVar varMaxApres = varsSmallestLB.get(0);
 		int maxApres = this.getTempsApres(varMaxApres);
@@ -107,23 +58,7 @@ class Strategie extends AbstractStrategy<IntVar>{
 				varMaxApres = var;
 			}
 		}
-		
-		if (boolAdd)
-			addNode(new Node(ordreInstanciation, varsSmallestLB));
-		
-		return new IntVar[] {varMaxApres, varMaxApres};
-	}
-	
-	private void addNode(Node node) {
-		int i = 0;
-		while (i < varsVarsSmallestLB.size() && this.sameOrderValueUpToL1(varsVarsSmallestLB.get(i).instancies, node.instancies))
-			i ++;
-		if (i > 0 && this.sameOrderValue(varsVarsSmallestLB.get(i - 1).instancies, node.instancies)) {
-			varsVarsSmallestLB.set(i - 1, node);
-		}
-		else {
-			varsVarsSmallestLB.add(node);
-		}
+		return varMaxApres;
 	}
 	
 	private IntVar getStationAct(String act) {
@@ -150,28 +85,16 @@ class Strategie extends AbstractStrategy<IntVar>{
 		if(d==null) d = new IntDecision(this.pool);
 		IntVar next = null;
 		
-		int i0 = ordreInstanciation.size() - 1;
-		int i = ordreInstanciation.size() - 1;
-		while (i >= 0 && ordreInstanciation.get(i).isInstantiatedTo(valInstanciation.get(i))){
-			ordreInstanciation.removeLast();
-			valInstanciation.removeLast();
-			i --;
-		}
 		
-		boolean inverseOp = false;
 		int valNext = 0;
 		if (prevDecision == null || prevDecision.getName().substring(0, 7).equals("Station")) {
-			IntVar[] res = this.getUninstanciatedWithSmallestLB();
-			prevDecision = res[0];
+			prevDecision = this.getUninstanciatedWithSmallestLB();
 			if (prevDecision == null) {
 				System.out.println(appels);
 				return null;
 			}
-			if (res[1] == null) inverseOp = true;
 			next = prevDecision;
 			valNext = prevDecision.getLB();
-			ordreInstanciation.add(next);
-			valInstanciation.add(valNext);
 		}
 		else if (prevDecision.getName().substring(0, 6).equals("Worker")) {
 			prevDecision = this.getStationAct(prevDecision.getName().substring(7));
@@ -195,13 +118,7 @@ class Strategie extends AbstractStrategy<IntVar>{
 		}
 		if (appels % 10000 == 0) System.out.println(appels);
 		appels ++;
-		if (!inverseOp)
-			d.set(next,valNext, DecisionOperatorFactory.makeIntEq());
-		else {
-			//System.out.println(appels + " : inegalite");
-			d.set(next, valNext, DecisionOperatorFactory.makeIntNeq());
-			d.setRefutable(false);
-		}
+		d.set(next,valNext, DecisionOperatorFactory.makeIntEq());
 	    return d;
     }
 	
